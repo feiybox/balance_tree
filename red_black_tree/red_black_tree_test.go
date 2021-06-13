9e0470c59fe9229662e46a4fcbf33d66b9909b27
feiybox <feiybox@foxmail.com>
2021-06-13 20:22:22 +0800

    feat: red_black_tree del node

--- red_black_tree/red_black_tree_test.go
+++ red_black_tree/red_black_tree_test.go
@@ -1,6 +1,10 @@
 package red_black_tree
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestFind(t *testing.T) {
 	root := &RBTreeNode{
@@ -32,15 +36,51 @@ func TestBFSPrint(t *testing.T) {
 }
 
 func TestAddNode(t *testing.T) {
-	root := AddNode(nil, 0)
-	for i := 1; i <= 400; i++ {
+	root := &RBTreeNode{
+		Val: 10,
+	}
+	for i := 0; i < 20; i++ {
 		root = AddNode(root, i)
 	}
+
 	for _, n := range BFSPrint(root) {
 		if n == nil {
 			t.Log("nil")
 			continue
 		}
-		t.Log(n.Val, n.Red)
+		p := n.parent
+		if p == nil {
+			t.Log(n.Val, n.Red, "nil")
+		} else {
+			t.Log(n.Val, n.Red, p.Val)
+		}
 	}
+}
+
+func TestDelNode(t *testing.T) {
+	root := &RBTreeNode{
+		Val: 10,
+	}
+	for i := 0; i < 20; i++ {
+		root = AddNode(root, i)
+	}
+	for i := 0; i < 10; i++ {
+		rand.Seed(time.Now().UnixNano())
+		root = DelNode(root, rand.Intn(20))
+	}
+
+	for _, n := range BFSPrint(root) {
+		if n == nil {
+			t.Log("nil")
+			continue
+		}
+		p := n.parent
+		if p == nil {
+			t.Log(n.Val, n.Red, "nil")
+		} else {
+			t.Log(n.Val, n.Red, p.Val)
+		}
+
+	}
+	t.Log("done")
 }
